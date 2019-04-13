18aab70e5c4c5858d8479981224eff2b762e0ee5
fosemberg <fosemberg@gmaill.com>
2019-04-13 23:11:57 +0300

    openable Param

--- src/components/Params/components/Param/Param.tsx
+++ src/components/Params/components/Param/Param.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
-import {FaAngleDown, FaPen} from "react-icons/fa";
+import {FaAngleDown, FaAngleUp, FaPen} from "react-icons/fa";
 import {IParam} from "../../../../store/params";
 import Relation from "./components/Relation/container";
 
-const Param = ({name, value, measureUnit, description, relations}: IParam) =>
-  <React.Fragment>
-    <tr>
-      <td>{name}</td>
-      <td>{measureUnit}</td>
-      <td>{description}</td>
-      <td>{value}</td>
-      <td><FaPen/></td>
-      <td>
+interface IState {
+  isOpen: boolean;
+}
+
+class Param extends React.PureComponent<IParam, IState> {
+  constructor(props: IParam) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  public render() {
+    const {
+      state: {isOpen},
+      props: {name, value, measureUnit, description, relations}
+    } = this
+    return (
+      <React.Fragment>
+        <tr>
+          <td>{name}</td>
+          <td>{measureUnit}</td>
+          <td>{description}</td>
+          <td>{value}</td>
+          <td><FaPen/></td>
+          <td>
+            {
+              relations &&
+              <span onClick={this.toggleOpen}>
+                {
+                  isOpen
+                    ? <FaAngleUp/>
+                    : <FaAngleDown/>
+                }
+              </span>
+            }
+          </td>
+        </tr>
         {
-          relations && <FaAngleDown/>
+          relations && isOpen &&
+          relations.map((relation, key) => <Relation {...{key, relation}}/>)
         }
-      </td>
-    </tr>
-    {
-      relations &&
-      relations.map((relation, key) => <Relation {...{key, relation}}/>)
-    }
-  </React.Fragment>
+      </React.Fragment>
+    );
+  }
+
+  private toggleOpen = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    })
+  }
+}
 
 export default Param;
