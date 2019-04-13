04e6840376759f9664e6857282759f4cfde37a2a
fosemberg <fosemberg@gmaill.com>
2019-04-13 22:28:49 +0300

    delete unused react logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,10 @@ import * as React from 'react';
 import './App.css';
 import Params from "./components/Params/container";
 
-import logo from './logo.svg';
-
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Params/>
       </div>
     );
